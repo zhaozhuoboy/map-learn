7862a49538e800f5d6ea7b04a1d7f8fd3537dc66
zhaozhuoboy <zhaozhuoboy@163.com>
2017-09-26 16:46:35 +0800

    a

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import L from 'leaflet';
+import 'leaflet-draw'
+import 'leaflet-draw/dist/leaflet.draw.css'
 import './App.css';
 
 class App extends Component {
   componentDidMount(){
-    var m = L.map('map').setView([37.92388861359015,115.22048950195312], 16);
-    L.tileLayer('https://api.mapbox.com/styles/v1/zhaozhuodev/cj6wxmlcb0jz92qqorge0ew7n/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiemhhb3podW9kZXYiLCJhIjoiY2o2d3g2NWI3MWtndzJ3cGRrbWc1MjQ4diJ9.SCM25AWgFuLBKdqSi3XFag', {
+    console.log(L)
+    var map = L.map('map',{drawControl: true}).setView([37.92388861359015,115.22048950195312], 16);
+    L.tileLayer('https://api.mapbox.com/styles/v1/zhaozhuodev/cj6x6z0799sbk2so4ggx2rqhb/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiemhhb3podW9kZXYiLCJhIjoiY2o2d3g2NWI3MWtndzJ3cGRrbWc1MjQ4diJ9.SCM25AWgFuLBKdqSi3XFag', {
       maxZoom: 18,
-      attribution: 'zhaozhuoboy &copy; <a href="http://github.com/zhaozhuoboy">zhaozhuodev</a>',
+      attribution: '&copy; <a href="http://github.com/zhaozhuoboy">zhaozhuodev</a>',
       id: 'mapbox.streets'
-    }).addTo(m);
-    m.on('click',(e)=>{
+    }).addTo(map);
+    var drawnItems = new L.FeatureGroup();
+    map.addLayer(drawnItems);
+    // var drawControl = new L.Control.Draw({
+    //     edit: {
+    //         featureGroup: drawnItems
+    //     }
+    // });
+    // map.addControl(drawControl);
+    map.on('click',(e)=>{
       console.log(e.latlng)
     })
-    var marker = L.marker([37.92388861359015,115.22048950195312]).addTo(m);
   }
   render() {
     return (
