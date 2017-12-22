c37348ff6307f7a6b8556a9b3c3a1155d6746646
zhaozhuoboy <zhaozhuoboy@163.com>
2017-12-22 11:20:03 +0800

    'a'

--- src/App.js
+++ src/App.js
@@ -1,149 +1,148 @@
 import React, { Component } from 'react';
 import L from 'leaflet';
 import 'leaflet-draw';
-import axios from 'axios';
-import HeatmapOverlay from 'leaflet-heatmap';
 import 'leaflet-draw/dist/leaflet.draw.css'
 import './App.css';
-import markerIcon from './images/point.png'
-import mkshadow from './images/shadow.png'
+// import markerIcon from './images/point.png'
+// import mkshadow from './images/shadow.png'
 
 
-let api = 'http://134.175.63.128:58900/common/queryGridByXY';
-var CancelToken = axios.CancelToken;
 let gridLayer = new L.layerGroup();
 let circleLayer = new L.layerGroup();
-var timer = null;
 class App extends Component {
   constructor(){
     super()
     this.map=null;
     this.prevlatlng=null;
-	this.staticLang=null;
-	this.DrawPolygon=false;
-	this.areaSelect=null;
-    
+    this.staticLang=null;
+    this.DrawPolygon=false;
+    this.areaSelect=null;
   }
-  sendRequest=(bounds)=>{
-    if(timer){
-      clearTimeout(timer)
-    }
-    timer = setTimeout(()=>{
-      console.log('发起请求..')
-    },800)
-  }
+ 
   componentDidMount(){
     this.initMap();
-    
-    let circle = L.circle([25.795022753899683,113.02887797355653],{
-      color: "red",
-       weight: 1,
-       radius: 20
-    });
-    let grid = L.polygon(returnGrid(data[0][3],data[0][4]));
-    L.featureGroup([circle,grid])
-    .bindPopup(`<button>adfssad</button>`)
-    .on('click', function(e) {
-
-    })
-    .addTo(this.map);
-    // this.map.on('click',(e)=>{
-    //   console.log(e.latlng)
-    //   var random = Math.floor(Math.random()*100+1);
-    //   const data={
-    //     ...e.latlng,
-    //     count:random
-    //   }
-    //   this.heatmapLayer.addData(data);
-    // })
     this.map.setView([25.792628,113.032208])
-   
-    this.map.on('dragstart',(ev)=>{
-      this.prevlatlng = this.map.getBounds();
-      this.staticLang = Math.abs(this.prevlatlng._northEast.lat-this.prevlatlng._southWest.lat);
-      console.log('--->',this.staticLang)
-	})
-	//area select function
-	let arr = [];
-	this.map.on('click', (ev) => {
-		  if (this.DrawPolygon) {
-			  //把点的坐标记录下来
-			  arr.push(ev.latlng);
-        let line = L.polyline(arr, {
-          weight: 1,
-          color: 'red',
-        }).addTo(this.map);
-			  //e.latlng
-			  //自定义marker图片
-			  let marker = new L.Marker(ev.latlng, {
-				  icon: new L.icon({
-					  iconUrl: markerIcon,
-					  shadowUrl: mkshadow,
-					  iconSize: [10, 10],
-					  shadowSize: [11, 3],
-					  iconAnchor: [5,5],
-					  shadowAnchor: [5, 2],
-				  })
-			  }).addTo(this.map)
-			  marker.on('click', (ev) => {
-				  console.log(ev)
-				  this.DrawPolygon = false;
-				  arr.push(ev.latlng);
-				  this.areaSelect = L.polygon(arr).addTo(this.map);
-				  arr = [];
-				  this.areaSelect.on('click', (ev) => {
-            console.log('area--->', ev)
-            console.log(this.areaSelect.toGeoJSON())
-            let bounds = this.areaSelect.getBounds();
-            console.log(bounds)
-            let a = bounds._northEast.lng + ' ' + bounds._northEast.lat;
-            let b = bounds._southWest.lng + ' ' + bounds._southWest.lat;
-            console.log(a,b)
-            console.log(toGridList(a,b))
-            L.polygon(toGridList(a,b)).addTo(this.map)
-				  })
-			  })
-		  }
-	  })
-    //拖拽结束事件
-    this.map.on('dragend',(ev)=>{
-      const nextLatLng = this.map.getBounds();
-      // let zoom = this.map.getZoom();113.023024 25.796699999999998", "113.02352 25.79715
-      //getBounds  获取地图边界  数据是 latlngBounds 对象
-      // 使用 latLng 方法将经纬度转换成  latLng点对象
-      // contains 方法 返回  点，或者矩形是否在  地图边界内
-      // let Bounds=this.map.getBounds();
-      let a= L.latLng(25.796699999999998,113.023024);
-      let b = L.latLng(25.79715,113.02352)
-      // console.log(Bounds.contains(L.latLngBounds(a,b)))
-      // console.log('拖动之前-->',this.prevlatlng)
-      // console.log('拖动之后--->',Bounds)
-      // let obj = {
-      //     leftlog:Bounds._southWest.lng,
-      //     leftlat:Bounds._southWest.lat,
-      //     rightlog:Bounds._northEast.lng,
-      //     rightlat:Bounds._northEast.lat,
-      //     FREQ_DL:'1.8G',
-      //     kpi:'rsrp'
-      // }
-      // this.sendRequest(Bounds)
-      // axios.post(api+`/20171004`,obj,{
-      //   cancelToken: source.token
-      // })
-      // .then(res=>{
-      //   console.log(res.data)
-      // })
-      // .catch(function(thrown) {
-      //   if (axios.isCancel(thrown)) {
-      //     console.log('Request canceled', thrown.message);
-      //   } else {
-      //     // 处理错误
-      //   }
-	  // })
-      
-    })
+    //初始化 Leaflet Draw 的文案
+    this.initDrawLocal();
+    // console.log(L.drawLocal)
+
   }
-  
+  initDrawLocal=()=>{
+    L.drawLocal = {
+      // format: {
+      // 	numeric: {
+      // 		delimiters: {
+      // 			thousands: ',',
+      // 			decimal: '.'
+      // 		}
+      // 	}
+      // },
+      draw: {
+        toolbar: {
+          actions: {
+            title: 'Cancel drawing',
+            text: 'Cancel'
+          },
+          finish: {
+            title: 'Finish drawing',
+            text: 'Finish'
+          },
+          undo: {
+            title: 'Delete last point drawn',
+            text: 'Delete last point'
+          },
+          buttons: {
+            polyline: 'Draw a polyline',
+            polygon: '画一个多边形',
+            rectangle: 'Draw a rectangle',
+            circle: '画一个圆',
+            marker: '标记',
+            circlemarker: '圆标记'
+          }
+        },
+        handlers: {
+          circle: {
+            tooltip: {
+              start: 'Click and drag to draw circle.'
+            },
+            radius: 'Radius'
+          },
+          circlemarker: {
+            tooltip: {
+              start: 'Click map to place circle marker.'
+            }
+          },
+          marker: {
+            tooltip: {
+              start: 'Click map to place marker.'
+            }
+          },
+          polygon: {
+            tooltip: {
+              start: 'Click to start drawing shape.',
+              cont: 'Click to continue drawing shape.',
+              end: 'Click first point to close this shape.'
+            }
+          },
+          polyline: {
+            error: '<strong>Error:</strong> shape edges cannot cross!',
+            tooltip: {
+              start: '开始',
+              cont: '继续点击地图画线',
+              end: '结束'
+            }
+          },
+          rectangle: {
+            tooltip: {
+              start: 'Click and drag to draw rectangle.'
+            }
+          },
+          simpleshape: {
+            tooltip: {
+              end: 'Release mouse to finish drawing.'
+            }
+          }
+        }
+      },
+      edit: {
+        toolbar: {
+          actions: {
+            save: {
+              title: 'Save changes',
+              text: 'Save'
+            },
+            cancel: {
+              title: 'Cancel editing, discards all changes',
+              text: 'Cancel'
+            },
+            clearAll: {
+              title: 'Clear all layers',
+              text: 'Clear All'
+            }
+          },
+          buttons: {
+            edit: 'Edit layers',
+            editDisabled: 'No layers to edit',
+            remove: 'Delete layers',
+            removeDisabled: 'No layers to delete'
+          }
+        },
+        handlers: {
+          edit: {
+            tooltip: {
+              text: 'Drag handles or markers to edit features.',
+              subtext: 'Click cancel to undo changes.'
+            }
+          },
+          remove: {
+            tooltip: {
+              text: 'Click on a feature to remove.'
+            }
+          }
+        }
+      }
+    };
+  }
   initMap(){
    
     const mapOption ={
@@ -156,46 +155,41 @@ class App extends Component {
       subdomains: "1234",
       attribution: '高德地图'
     }).addTo(this.map);
+
     var drawnItems = new L.FeatureGroup();
-    // this.map.addLayer(drawnItems);
+    this.map.addLayer(drawnItems);
     var drawControl = new L.Control.Draw({
       draw: {
-        polyline:{
-          metric:true,
-          repeatMode:false,
-          shapeOptions:{
-            color: '#000',
-            weight: 10,
-          }
-        }
-        
+        position: 'topleft'
       },
-      polygon:false,
-      rectangle:false,
-      circle:false,
       edit: {
         featureGroup: drawnItems
       }
     });
     this.map.addControl(drawControl);
     
-
   }
+  // //测量
+  // measure=()=>{
+  //     // var stopclick = false; //to prevent more than one click listener
+
+  //   var Polygon = new L.Draw.Polygon(this.map, {
+  //       shapeOptions: {
+          
+  //       }
+  //     });
+  //     Polygon.enable();
+
+  // }
 	  
-  	click=()=>{
-		this.DrawPolygon = !this.DrawPolygon;
-		alert(this.DrawPolygon)
-		
-	}
-  
 	render() {
 		return (
 		<div>
 				<div className="App" id='map'>
 				</div>
-				<button style={{position: 'absolute',bottom: 10}} onClick={this.click}>
-					画区域
-				</button>
+        {/* <button style={{ position: 'absolute', bottom: 10 }}onClick={this.measure}>
+          画区域
+				</button> */}
 		</div>
 		);
 	}
@@ -204,29 +198,17 @@ class App extends Component {
 export default App;
 
 
-function returnGrid(a,b){
-  let a1 = Number(a.split(' ')[1]);
-  let b1 = Number(a.split(' ')[0]);
-  let a2 = Number(b.split(' ')[1]);
-  let b2 = Number(b.split(' ')[0]);
-  let one =[a1,b1];
-  let two =[a1,b2];
-  let three = [a2,b2];
-  let four = [a2,b1];
-  let five =[a1,b1];
-  return [one,two,three,four,five];
-}
-function toGridList(a, b) {
-  let a1 = Number(a.split(' ')[1]);
-  let b1 = Number(a.split(' ')[0]);
-  let a2 = Number(b.split(' ')[1]);
-  let b2 = Number(b.split(' ')[0]);
-  let one = [a1, b1];
-  let two = [a1, b2];
-  let three = [a2, b2];
-  let four = [a2, b1];
-  let five = [a1, b1];
-  return [one, two, three, four, five];
-}
+// function returnGrid(a,b){
+//   let a1 = Number(a.split(' ')[1]);
+//   let b1 = Number(a.split(' ')[0]);
+//   let a2 = Number(b.split(' ')[1]);
+//   let b2 = Number(b.split(' ')[0]);
+//   let one =[a1,b1];
+//   let two =[a1,b2];
+//   let three = [a2,b2];
+//   let four = [a2,b1];
+//   let five =[a1,b1];
+//   return [one,two,three,four,five];
+// }
 
-let data =[[-89.6921,-89.25,-94.9417,"113.02848 25.7949","113.028976 25.79535"],[-91.8575,null,-106.665,"113.029472 25.794","113.029968 25.79445"],[-95.256,-93.0,-98.5233,"113.029472 25.794449999999998","113.029968 25.7949"],[-97.725,-72.0,-83.0,"113.02848 25.79265","113.028976 25.7931"],[-88.4233,-101.0,-94.25,"113.027488 25.792199999999998","113.027984 25.79265"],[-83.818,-94.946,-92.3013,"113.03096000000001 25.792199999999998","113.031456 25.79265"],[-102.0233,-107.5,-105.3,"113.027984 25.792199999999998","113.02848 25.79265"],[-90.196,-90.55,-100.7133,"113.028976 25.794449999999998","113.029472 25.7949"],[-88.5663,-93.545,-97.246,"113.03046400000001 25.79355","113.03096000000001 25.794"],[-89.1808,-90.995,-96.39,"113.03096000000001 25.79355","113.031456 25.794"],[-81.1895,-88.195,-89.1514,"113.03046400000001 25.794","113.03096000000001 25.79445"],[-116.0,null,null,"113.03096000000001 25.79355","113.031456 25.794"],[-90.165,-100.645,-98.565,"113.03046400000001 25.79265","113.03096000000001 25.7931"],[-87.3827,-91.32,-93.3483,"113.03096000000001 25.7949","113.031456 25.79535"],[-74.25,null,-74.0,"113.03096000000001 25.7931","113.031456 25.79355"],[-85.4967,-94.43,-96.65,"113.029472 25.792199999999998","113.029968 25.79265"],[-103.0,null,null,"113.029472 25.7949","113.029968 25.79535"],[-83.8119,-93.648,-90.8663,"113.03046400000001 25.794449999999998","113.03096000000001 25.7949"],[-94.62,-101.6657,-99.2938,"113.02996800000001 25.794449999999998","113.03046400000001 25.7949"],[-92.6,-117.0,-99.5,"113.027984 25.79265","113.02848 25.7931"],[-85.743,-96.1056,-92.3618,"113.03096000000001 25.79265","113.031456 25.7931"],[-84.4995,-85.8125,-91.016,"113.03096000000001 25.7931","113.031456 25.79355"],[-92.1167,-105.0,-98.71,"113.029472 25.79355","113.029968 25.794"],[-91.9214,-80.63,-96.225,"113.031456 25.794449999999998","113.031952 25.7949"],[-94.0,null,-102.0,"113.027488 25.794","113.027984 25.79445"],[-110.0,null,null,"113.03046400000001 25.794","113.03096000000001 25.79445"],[-95.8925,-99.325,-103.01,"113.02996800000001 25.794","113.03046400000001 25.79445"],[-82.3608,-92.63,-92.48,"113.028976 25.7949","113.029472 25.79535"],[-92.825,-96.5433,-93.4,"113.031456 25.79355","113.031952 25.794"],[-94.81,-112.875,-92.6467,"113.029472 25.79265","113.029968 25.7931"],[-97.9325,null,-113.63,"113.02848 25.7931","113.028976 25.79355"],[-86.0407,-89.4,-87.8817,"113.02996800000001 25.7949","113.03046400000001 25.79535"],[-96.4375,-89.0,-81.0,"113.02848 25.79355","113.028976 25.794"],[-97.47,-75.38,-88.11,"113.02848 25.792199999999998","113.028976 25.79265"],[-97.745,-106.2,-90.085,"113.028976 25.794","113.029472 25.79445"],[-98.0,null,null,"113.03096000000001 25.792199999999998","113.031456 25.79265"],[-99.7,-114.0,-106.5,"113.027488 25.79265","113.027984 25.7931"],[-87.6,-78.1467,-89.065,"113.027984 25.7949","113.02848 25.79535"],[-90.8,-111.0,-112.0,"113.02996800000001 25.79355","113.03046400000001 25.794"],[-93.37,-113.0,-98.25,"113.028976 25.7931","113.029472 25.79355"],[-97.48,-114.25,-111.97,"113.02996800000001 25.79265","113.03046400000001 25.7931"],[-88.6667,-91.0,-91.5,"113.028976 25.79355","113.029472 25.794"],[-83.55,-97.4529,-92.695,"113.03046400000001 25.7949","113.03096000000001 25.79535"],[-90.3357,-78.49,-90.855,"113.027488 25.794449999999998","113.027984 25.7949"],[-115.25,-107.5,-120.0,"113.027488 25.79355","113.027984 25.794"],[-89.308,-93.8475,-92.91,"113.031456 25.792199999999998","113.031952 25.79265"],[-87.497,-90.695,-92.9467,"113.03046400000001 25.7931","113.03096000000001 25.79355"],[-89.6075,-118.0,-96.72,"113.031456 25.79265","113.031952 25.7931"],[-83.1467,-90.0617,-90.79,"113.029472 25.7949","113.029968 25.79535"],[-87.2415,-87.638,-95.535,"113.027488 25.7949","113.027984 25.79535"],[-86.75,null,null,"113.027984 25.794","113.02848 25.79445"],[-98.064,-89.4,-101.5233,"113.028976 25.79265","113.029472 25.7931"],[-88.654,-96.82,-93.21,"113.03046400000001 25.792199999999998","113.03096000000001 25.79265"],[-94.45,-109.0,-111.5,"113.02996800000001 25.7931","113.03046400000001 25.79355"],[-111.0,null,-111.0,"113.027984 25.7931","113.02848 25.79355"],[-93.01,-93.5,-97.9167,"113.031456 25.7931","113.031952 25.79355"],[-79.5146,-79.195,-83.086,"113.031456 25.7949","113.031952 25.79535"],[-86.358,-91.0288,-94.5667,"113.03096000000001 25.794","113.031456 25.79445"],[-95.16,-80.15,-96.65,"113.028976 25.792199999999998","113.029472 25.79265"],[-95.036,-107.25,-92.08,"113.029472 25.7931","113.029968 25.79355"],[-107.2767,null,null,"113.027984 25.79355","113.02848 25.794"],[-86.5567,-76.45,-92.295,"113.027984 25.794449999999998","113.02848 25.7949"],[-97.125,-101.13,-94.47,"113.02996800000001 25.792199999999998","113.03046400000001 25.79265"],[-90.33,null,null,"113.02848 25.794","113.028976 25.79445"],[-95.8633,-89.5675,-95.636,"113.031456 25.794","113.031952 25.79445"],[-82.4307,-91.93,-97.39,"113.03096000000001 25.794449999999998","113.031456 25.7949"],[-83.584,-78.0,-89.2,"113.02848 25.794449999999998","113.028976 25.7949"]]
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route,Router,hashHistory,browserHistory} from 'react-router';
+import { Route,Router,browserHistory} from 'react-router';
 import './index.css';
 import '../node_modules/leaflet/dist/leaflet.css'
 import App from './App';
-import Sinomap from './sinomap'
-import FlipMoveDemo from './FlipMoveDemo';
 
 var routes=()=>(
     <Router history={browserHistory}>
-        <Route path='/' component={Sinomap}/>
-        <Route path='/map' component={App}/>
-        <Route path='/move' component={FlipMoveDemo} />
+        <Route path='/' component={App}/>
     </Router>
 )
 ReactDOM.render(routes(), document.getElementById('root'));
